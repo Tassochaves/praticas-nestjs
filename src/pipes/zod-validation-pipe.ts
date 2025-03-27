c80a2308927c465b916e2fa122828acb1d534db8
Tassochaves <tassogt@hotmail.com>
2025-03-27 08:28:17 -0300

    Feat: implements ZodValitation

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+
+import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { ZodError, ZodSchema  } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value);
+      
+    } catch (error) {
+        if(error instanceof ZodError){
+            throw new BadRequestException({
+                message: 'Validantion failed',
+                statusCode: 400,
+                errors: error.format(),
+                
+            })
+        }
+      throw new BadRequestException('Validation failed');
+    }
+
+    return value;
+  }
+}
